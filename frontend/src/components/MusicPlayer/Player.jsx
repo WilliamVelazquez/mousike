4b733cf5be031ddb54070ca1062f42e96ddf5252
David Behar <davbelom@gmail.com>
2019-11-11 03:47:22 -0600

    Player in styled component

--- frontend/src/components/MusicPlayer/Player.jsx
+++ frontend/src/components/MusicPlayer/Player.jsx
@@ -3,8 +3,68 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable react/no-string-refs */
 import React from 'react';
+import styled from 'styled-components';
 import './Player.css';
 // import loadFiles from '../files';
+
+const AudioPlayer = styled.audio``;
+const PlayerWrapper = styled.div`
+    margin: 15px 0;
+    /* background-color: #052b52; */
+    width: 100%;
+    height: 50px;
+    position: relative;
+    display: grid;
+    grid-template-columns: 110px 1fr;
+    grid-gap: 10px;
+`;
+const Controls = styled.div`
+    float: left;
+`;
+const ControlsButton = styled.button`
+  background: none;
+  border: none;
+
+  text-decoration: none;
+  height: 50px;
+  width: 34px;
+  box-sizing: border-box;
+  text-align: center;
+  padding: 17px 10px;
+  float: left;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #084380;
+    color: #b3b3b3;
+  } 
+`;
+
+const Progress = styled.div`
+    position: absolute;
+    left: 102px;
+    top: 15px;
+    right: 100px;
+    bottom: 15px;
+    background: black;
+    cursor: pointer;
+    /* width: 100%; */
+`;
+const Bar = styled.div`
+    position: absolute;
+    left: 5px;
+    right: 5px;
+    top: 5px;
+    bottom: 5px;
+    & > div{
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 50%;
+      background: white;    
+    }
+`;
 
 class Player extends React.Component {
   constructor() {
@@ -23,18 +83,6 @@ class Player extends React.Component {
   nextSongHandler(event) {
     event.preventDefault();
     alert('b');
-  }
-
-  play(event) {
-    const { player } = this.refs;
-    player.play();
-    event.preventDefault();
-  }
-
-  pause(event) {
-    const { player } = this.refs;
-    player.pause();
-    event.preventDefault();
   }
 
   togglePlay() {
@@ -57,37 +105,37 @@ class Player extends React.Component {
     const { playing } = this.state;
     const { file } = this.state;
     return (
-      <div className='player'>
-        <div className='controls'>
-          <button type='button' onClick={this.previousSongHandler.bind(this)}>
+      <PlayerWrapper className='player'>
+        <Controls className='controls'>
+          <ControlsButton type='button' onClick={this.previousSongHandler.bind(this)}>
             {' '}
             <i className='fa fa-chevron-left' aria-hidden='true' />
-          </button>
-          <button type='button' onClick={this.togglePlay.bind(this)}>
+          </ControlsButton>
+          <ControlsButton type='button' onClick={this.togglePlay.bind(this)}>
             {' '}
             <i className={playing ? 'fa fa-pause' : 'fa fa-play'} aria-hidden='true' />
-          </button>
-          <button type='button' onClick={this.nextSongHandler.bind(this)}>
+          </ControlsButton>
+          <ControlsButton type='button' onClick={this.nextSongHandler.bind(this)}>
             {' '}
             <i className='fa fa-chevron-right' aria-hidden='true' />
-          </button>
-        </div>
+          </ControlsButton>
+        </Controls>
         <div>
-          <div className='progress'>
-            <div className='bar'>
+          <Progress className='progress'>
+            <Bar className='bar'>
               <div />
-            </div>
-          </div>
+            </Bar>
+          </Progress>
 
         </div>
-        <audio ref='player'>
+        <AudioPlayer ref='player'>
           <source src={file} />
           <track
             default
             kind='captions'
           />
-        </audio>
-      </div>
+        </AudioPlayer>
+      </PlayerWrapper>
     );
   }
 }
