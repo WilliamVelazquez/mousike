047f3acc03a4964c6cfb9c7892f5304697147b4f
David Behar <davbelom@gmail.com>
2019-11-12 16:09:24 -0600

    updated folder tree

---
+++ ssr-frontend/.storybook/webpack.config.js
@@ -0,0 +1,29 @@
+
+/**
+ * @author William Velazquez <info@williamvelazquez.com>
+ */
+console.log('Cargando el webpack para storybook...');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
+  config.module.rules.push({
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+      'babel-loader',
+    ],
+  });
+  return config;
+};
