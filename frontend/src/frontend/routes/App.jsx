a2900f8ca3b35970e62f793ad8135cb04c8e81c9
David Behar <davbelom@gmail.com>
2019-11-13 23:46:33 -0600

    redux thunk installed

--- frontend/src/frontend/routes/App.jsx
+++ frontend/src/frontend/routes/App.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  // BrowserRouter,
+  Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 // import Layout from '../components/Layout';
+import { createBrowserHistory } from 'history';
 import NotFound from '../containers/NotFound';
 import Discover from '../containers/Discover';
 import FullMusicPlayer from '../components/FullMusicPlayer/FullMusicPlayer';
+// import Layout from '../components/Layout';
 
-const App = () => (
-  <BrowserRouter>
-    {/* <Layout> */}
-    <Switch>
-      <Route exact path="/" component={Discover} />
-      <Route exact path="/discover" component={Discover} />
-      <Route exact path="/player" component={FullMusicPlayer} />
-      <Route component={NotFound} />
-    </Switch>
-    {/* </Layout> */}
-  </BrowserRouter>
-);
+const App = () => {
+  const history = createBrowserHistory();
+
+  return (
+    <Router history={history}>
+      {/* behagoras if uncomment the images stop loading */}
+      {/* <Layout> */}
+      <Switch>
+        <Route exact path="/" component={Discover} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/player" component={FullMusicPlayer} />
+        <Route component={NotFound} />
+      </Switch>
+      {/* </Layout> */}
+    </Router>
+  );
+};
 
 export default App;
