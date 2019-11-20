b60768814b305a54384a0e56c186ac3b6126f6c8
William Velazquez <william.velazquez.a@gmail.com>
2019-11-19 20:49:32 -0600

    Updating LoginLayout Component

--- frontend/src/frontend/components/LoginLayout/index.jsx
+++ frontend/src/frontend/components/LoginLayout/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import GoogleIcon from '../Icons/GoogleIcon';
+import TwitterIcon from '../Icons/TwitterIcon';
+import FacebookIcon from '../Icons/FacebookIcon';
 import SocialMediaLoginButton from '../SocialMediaLoginButton';
 
 const Layout = styled.div`
   display: grid;
   margin-top: 50px;
+  padding-bottom: 60px;
   justify-content: center;
 `;
 
@@ -12,7 +16,7 @@ const BackgroundBox = styled.div`
   padding: 40px;
   min-width: 400px;
   border-radius: 20px;
-  background-color: black;
+  background-color: #333333;
 `;
 const SocialMediaButtonsBox = styled.div`
   display: grid;
@@ -27,14 +31,31 @@ const SignUpBox = styled.div`
 `;
 
 const LoginLayout = () => {
-
   return (
     <Layout>
       <BackgroundBox>
         <SocialMediaButtonsBox>
-          <SocialMediaLoginButton text="Login with Google" background="#ffffff" textColor="#262930" />
-          <SocialMediaLoginButton text="Login with Twitter" background="#1da1f2" />
-          <SocialMediaLoginButton text="Login with Facebook" background="#395697" />
+          <SocialMediaLoginButton text="Login with Google" background="#ffffff" textColor="#262930">
+            <GoogleIcon
+              size={18}
+              color="#262930"
+              hoverColor="#052A4F"
+            />
+          </SocialMediaLoginButton>
+          <SocialMediaLoginButton text="Login with Twitter" background="#1da1f2">
+            <TwitterIcon
+              size={18}
+              color="#fff"
+              hoverColor="#052A4F"
+            />
+          </SocialMediaLoginButton>
+          <SocialMediaLoginButton text="Login with Facebook" background="#395697">
+            <FacebookIcon
+              size={18}
+              color="#fff"
+              hoverColor="#052A4F"
+            />
+          </SocialMediaLoginButton>
         </SocialMediaButtonsBox>
         <SignUpBox>
           <SocialMediaLoginButton text="Sign up!" textColor="#262930" />
