07d8ea8dbee8b2feab42e2b9888880b72c65453f
William Velazquez <william.velazquez.a@gmail.com>
2019-11-19 21:01:06 -0600

    Updating Frontend Server Port Settings

--- frontend/src/server/config/index.js
+++ frontend/src/server/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 8000,
+  port: process.env.PORT || 3000,
   apiUrl: process.env.API_URL,
   apiKeyToken: process.env.API_KEY_TOKEN,
   googleClientId: process.env.GOOGLE_CLIENT_ID,
@@ -10,7 +10,7 @@ const config = {
   sessionSecret: process.env.SESSION_SECRET,
   twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
   twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  apiPort: process.env.API_PORT,
+  apiPort: process.env.API_PORT || 8080,
 };
 
 module.exports = { config };
