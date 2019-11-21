fca09a2a902a34c95caecace56a2854c1d07521c
William Velazquez <william.velazquez.a@gmail.com>
2019-11-20 18:30:10 -0600

    Ignoring BD PORT on Mongo Connection

--- api-server/lib/mongo.js
+++ api-server/lib/mongo.js
@@ -5,7 +5,8 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
+// const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
   constructor() {
